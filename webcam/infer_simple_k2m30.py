a9026201091b996513a82cbf1041cab42d030c4f
Mikhail Chuprynski <mikhail.chuprynski@gmail.com>
2018-03-25 17:11:54 +0300

    -

--- webcam/infer_simple_k2m30.py
+++ webcam/infer_simple_k2m30.py
@@ -76,8 +76,9 @@ def gen():
         )
         im = cv2.imread(str(n) + '.jpg')
         ret, jpeg = cv2.imencode('.jpg', im)
+        n += 1
         yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + jpeg.tobytes() + b'\r\n\r\n')
-        
+
 
 @app.route('/video_feed')
 def video_feed():
